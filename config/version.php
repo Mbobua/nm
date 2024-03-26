48f54e321ef888d03b6a021487ae73e61c96da18
William Githinji <w.githinji@centum.co.ke>
2024-03-26 14:33:56 +0300

    first

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.3.2',
+  'full_app_version' => 'v6.3.2 - build 12834-g9a5c1b812',
+  'build_version' => '12834',
+  'prerelease_version' => '',
+  'hash_version' => 'g9a5c1b812',
+  'full_hash' => 'v6.3.2-160-g9a5c1b812',
+  'branch' => 'master',
+);
